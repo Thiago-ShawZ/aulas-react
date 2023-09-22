5de926f1beb51c0a9416597c135b921b5199d167
Thiago <thiago.costa.nascimento@eaportal.org>
2023-09-22 15:26:35 -0300

    projeto_finalizado

--- 2_MENU_RESPONSIVO/src/App.jsx
+++ 2_MENU_RESPONSIVO/src/App.jsx
@@ -2,7 +2,13 @@ import Header from "./Components/Header"
 
 function App() {
   return (
+    <div>
     <Header />
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, eos reprehenderit possimus animi harum quaerat consequatur magni? Voluptas, quia iste exercitationem eaque quibusdam quidem distinctio quaerat minima animi neque error.
+    Sunt aut, aperiam unde consequatur illum doloremque fugit. Fuga veniam in odio illum dolorum reiciendis nesciunt inventore aliquid ipsam natus sint recusandae, repellat laudantium magni molestias similique ut! Sed, quibusdam!
+    Nulla assumenda, beatae eveniet ut quam aliquam illo quis possimus quos consequatur pariatur enim laudantium. Cupiditate quo magni, consequatur natus reiciendis soluta labore temporibus sequi architecto dolore quibusdam id quis.
+    Id illo dolores impedit praesentium aspernatur cum. Voluptate beatae sapiente vitae eveniet inventore doloribus, laudantium aperiam! Laudantium sed expedita recusandae perferendis! Fugiat facere mollitia, totam explicabo quisquam beatae at similique!</p>
+    </div>
   )
 }
 
